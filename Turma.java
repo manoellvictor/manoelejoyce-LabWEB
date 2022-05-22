ce5ad89b01e407d2b5a368cc5c7d964652e8722c
manoellvictor <51246952+manoellvictor@users.noreply.github.com>
2022-05-22 20:45:20 -0300

    Create Turma.java

---
+++ Turma.java
@@ -0,0 +1,50 @@
+package projeto1.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "turmas")
+public class Turma {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "sala")
+    private String sala;
+
+    @Column(name = "codigo")
+    private String codigo;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST})
+    @JsonIgnore
+    private Set<Aluno> alunos;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST})
+    @JsonIgnore
+    private Set<Professor> professores;
+
+    public Turma(String nome, String sala, String codigo) {
+        this.nome = nome;
+        this.sala = sala;
+        this.codigo = codigo;
+        this.alunos = new HashSet<>();
+        this.professores = new HashSet<>();
+    }
+    
+}
